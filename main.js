3cfa8262c7aaea637449cfad6d6bf3dcf1f3d17f
ferreterialashuertasonline-sketch <ferreterialashuertasonline@gmail.com>
2026-09-23 10:04:16 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, shell } = require('electron');
 const path = require('path');
 
 function createWindow(){
@@ -14,6 +14,15 @@ function createWindow(){
   });
   win.setMenuBarVisibility(false);
   win.loadFile(path.join(__dirname, 'www-desktop', 'index.html'));
+
+  // Los enlaces mailto: (botón "Enviar pedido") deben abrir el programa
+  // de correo del sistema, no intentar cargarse dentro de la propia app.
+  win.webContents.on('will-navigate', (event, url) => {
+    if(url.startsWith('mailto:')){
+      event.preventDefault();
+      shell.openExternal(url);
+    }
+  });
 }
 
 app.whenReady().then(() => {
